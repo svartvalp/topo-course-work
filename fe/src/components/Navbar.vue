754f80f9be6c5a408aa2e288237b3c17dd9fce86
Kasyanenko Konstantin Vladimirovich <kkasyanenko@ozon.ru>
2021-12-11 19:16:01 +0300

    name

--- fe/src/components/Navbar.vue
+++ fe/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navbar navbar-dark bg-dark" >
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Могазин</router-link>
+      <router-link class="navbar-brand" to="/">Книголюбы</router-link>
       <div class="d-flex">
         <router-link to="/cart" class="btn btn-outline-success" >Корзина</router-link>
       </div>
